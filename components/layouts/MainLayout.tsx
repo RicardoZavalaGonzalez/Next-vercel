900b12771ed83c4af121c7092361536c56fcd33b
RicardoZavalaGonzalez <ricky_zg@hotmail.com>
2022-03-13 14:08:48 -0600

    app deploy in vercel

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react';
+import Head from 'next/head';
+import { Navbar } from '../Navbar';
+import styles from './MainLayout.module.css';
+
+export const MainLayout: FC = ({ children }) => {
+
+    return (
+        <div className={styles.container}>
+
+            <Head>
+                <title>Mi curso de NEXT</title>
+                <meta name="description" content="NEXT" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <Navbar />
+
+            <main className={styles.main}>
+
+                {children}
+
+            </main>
+        </div>
+    )
+}
